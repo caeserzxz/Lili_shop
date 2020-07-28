4f6c4beed15da679bdd973a7235db728b79f8d70
zhuoxuezhong <1062763849@qq.com>
2020-07-28 16:28:29 +0800

    优化了防漏收

--- application/http/controller/OrderMessage.php
+++ application/http/controller/OrderMessage.php
@@ -4,6 +4,7 @@ use think\worker\Server;
 use Workerman\Lib\Timer;
 use think\facade\Cache;
 use think\cache\driver\Redis;
+use app\unique\model\PayRecordModel;
 
 class OrderMessage extends Server
 {
@@ -13,7 +14,7 @@ class OrderMessage extends Server
     public function __construct()
     {
         $this->option = [
-            'count'		=> 4,
+            'count'		=> 1,
             'name'		=> 'OrderMessage'];
         parent::__construct();
     }
@@ -46,20 +47,27 @@ class OrderMessage extends Server
                 $data = $this->getBroadcast();
             // $connection->send($data);
         }
-        //获取临时数据
-        $list = Cache::get('temp_order_list');
-        //处理临时数据
+
+
         if(empty($_data['type'])){
+            $redis = new Redis();
+            //获取临时数据
+            $list = Cache::get('temp_order_list');
+
+            //这里接受安卓的调用成功的数据,删除临时数组里面已成功调起的数据
             foreach ($_data['data'] as $k=>$v){
                 foreach ($list as $key=>$value){
                     if($v['log_id']==$value['log_id']){
-                        unset($list[$key]);
+                        $list[$k]['num'] = $list[$k]['num']+1;
                     }
                 }
             }
+            //更新临时数据
+            Cache::set('temp_order_list',$list);
+
         }
-        //更新临时数据
-        Cache::set('temp_order_list',$list);
+
+
 
     }
     //断开连接
@@ -81,37 +89,72 @@ class OrderMessage extends Server
     //获取当前需要语音播报的订单
     public function getBroadcast(){
         $redis = new Redis();
-        $buyCount = $redis->Llen('broadcast');
         //获取临时数据
         $list = Cache::get('temp_order_list');
         if(empty($list)) $list = [];
 
+        //$data是最后返回广播的数组
         $data['data'] = [];
+
+        $buyCount = $redis->Llen('broadcast');
         for ($i=0;$i<=$buyCount-1;$i++){
             // $str = $redis->Lindex('broadcast',$i);
-
+            //取出并删除
             $str = $redis->rPop('broadcast');
             $arr = [];
             $arr = object_array(json_decode($str));
 
-            //当发送次数小于3的时候才会继续发送
-            if(empty($arr['num'])||$arr['num']<10){
-                if(empty($arr['num'])){
-                    $arr['num'] = 1;
-                }else{
-                    $arr['num'] =  $arr['num']+1;
-                }
-                array_push($list,$arr);
-
+            if(empty($arr['log_id'])){
+                continue;
             }
 
-            array_push($data['data'],$arr);
+            if(empty($arr['num'])){
+                $arr['num'] =1;
+            }else{
+                $arr['num'] = $arr['num']+1;
+            }
+
+            //如果当前播报次数超过限制则不再播报
+            if($arr['num']>=4){
+                //删除在临时数组中的数据
+                foreach ($list as $key=>$value){
+                    if($value['log_id']==$arr['log_id']){
+                        unset($list[$key]);
+                    }
+                }
+            }else{
+                //增加到广播的数组中
+                array_push($data['data'],$arr);
+
+                //更新临时数组中的次数 如果存在就更新  不存在就新增
+                $is_update = 1;
+                foreach ($list as $k=>$v){
+                    if($arr['log_id']==$v['log_id']){
+                        $list[$k] = $arr;
+                        $is_update = 2;
+                        break;
+                    }
+                }
+                if($is_update==1){
+                    array_push($list,$arr);
+                }
+            }
+
         }
         //存临时数组
         Cache::set('temp_order_list',$list);
+        //重新进入队列
+        foreach ($list as $k=>$v){
+            $str = json_encode($v);
+            $redis->rPush('broadcast',$str);
+        }
+
         $data['timestamp'] = time();
         $data_json =  json_encode($data,JSON_UNESCAPED_SLASHES);
         return $data_json;
 
     }
+    //取出队列里面的数组
+    //当次数小于3就播报并存数组
+    //再将数组里面的数据存入队列
 }
